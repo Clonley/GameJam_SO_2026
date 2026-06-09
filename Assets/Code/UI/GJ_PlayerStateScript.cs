bfd5e04c703bfa3bced5b41e6007735551041fc5
Leon Rathmann <rathmannlr@gmail.com>
2026-06-09 15:16:36 +0200

    PLEASE FUCKING COMMIT IT

---
+++ Assets/Code/UI/GJ_PlayerStateScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GJ_PlayerStateScript : MonoBehaviour
+{
+    public CanvasGroup m_deathImage;
+    public CanvasGroup m_blackScreen;
+    public float blackAnimationDuration = 1f;
+    public float imageAnimationDuration = 2f;
+
+    private void Start()
+    {
+        m_deathImage.alpha = 0f;
+        m_blackScreen.alpha = 0f;
+
+        m_blackScreen.gameObject.SetActive(false);
+        m_deathImage.gameObject.SetActive(false);
+    }
+    public void FuckingDie()
+    {
+        Debug.Log("Player has died");
+        StartCoroutine(DeathSequence());
+    }
+
+    public void EndGame()
+    {
+
+    }
+
+
+    IEnumerator DeathSequence()
+    {
+        Time.timeScale = 0;
+
+        m_blackScreen.gameObject.SetActive(true);
+        m_deathImage.gameObject.SetActive(true);
+
+        m_deathImage.alpha = 0f;
+        m_blackScreen.alpha = 0f;
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("MainMenu");
+
+        asyncLoad.allowSceneActivation = false;
+
+        float counter = 0f;
+
+        while (counter < Mathf.Max(blackAnimationDuration, imageAnimationDuration))
+        {
+            counter += Time.unscaledDeltaTime;
+            m_deathImage.alpha = Mathf.Lerp(0f, 1f, counter / imageAnimationDuration);
+            m_blackScreen.alpha = Mathf.Lerp(0f, 1f, counter / blackAnimationDuration);
+
+            yield return null;
+        }
+        while (asyncLoad.progress < 0.9f) yield return null;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        asyncLoad.allowSceneActivation = true;
+    }
+}
