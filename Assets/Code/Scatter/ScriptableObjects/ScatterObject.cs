a8d39751134d503dd45d22977c94ff3949970678
Leon Rathmann <rathmannlr@gmail.com>
2026-06-11 00:54:13 +0200

    RemovedVines

--- Assets/Code/Scatter/ScriptableObjects/ScatterObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ScatterObject", menuName = "Scatter/ScatterInstance")]
-public class ScatterObject : ScriptableObject
-{
-    [Header("LOD Settings")]
-    public List<ScatterObjectData> LODs = new List<ScatterObjectData>();
-}
